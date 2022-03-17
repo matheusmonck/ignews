3c138aae7f6bfdecbb75af3324749b63b590516d
Kalley Matheus <62453333+matheusmonck@users.noreply.github.com>
2022-03-17 13:13:04 -0300

    Component Header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  redner() {
-    return (
-      <Html lang='en'>
-        <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Roboto&display=swap'
-            rel='stylesheet'
-          />
-          <title>ig.news</title>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap'
+          rel='stylesheet'
+        />
+        <link rel='shortcut icon' href='/favicon.png' type='image/png' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
