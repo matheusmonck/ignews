1d91b597a1dfd0afcfeaedf3b7deb004a6ebd90d
Kalley Matheus <matheusmonck@gmail.com>
2022-04-14 10:57:47 -0300

    posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href=''>
+            <time>12 de março de 2021</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>
+              Neste post vamos desenvolver uma página web para demonstrar, na
+              prática, a integração de Mapas em uma aplicação com React usando
+              Leaflet.
+            </p>
+          </a>
+          <a href=''>
+            <time>12 de março de 2021</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>
+              Neste post vamos desenvolver uma página web para demonstrar, na
+              prática, a integração de Mapas em uma aplicação com React usando
+              Leaflet.
+            </p>
+          </a>
+          <a href=''>
+            <time>12 de março de 2021</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>
+              Neste post vamos desenvolver uma página web para demonstrar, na
+              prática, a integração de Mapas em uma aplicação com React usando
+              Leaflet.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
